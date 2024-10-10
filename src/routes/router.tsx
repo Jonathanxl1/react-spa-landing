a8e300a155c05f7367e28b3b7ad94a1ac3232531
Jonathan A <jonathanxl1@hotmail.com>
2024-10-09 20:22:46 -0500

    feat:Define Layout Page and default page loaded

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,3 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout";
+import Home from "../pages/Home";
 export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+    ],
+  },
 ]);
