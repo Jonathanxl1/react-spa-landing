8065c6df0ca5e73fb87cf27a2797870320968186
Jonathan A <jonathanxl1@hotmail.com>
2024-10-09 19:04:55 -0500

    feat:Base config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-64 h-48 bg-red-400">
+        <p>hello world</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
