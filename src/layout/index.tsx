ac2c962106f0ca2739d4f30c48572490ff02e2f3
Jonathan A <jonathanxl1@hotmail.com>
2024-10-10 17:26:05 -0500

    feat:Added Navbar to layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 function Layout(): JSX.Element {
   return (
     <>
+      <Navbar />
       <main className="w-full h-auto min-h-64 bg-gradient-to-b from-white from-20% to-green-600">
         <Outlet />
       </main>
