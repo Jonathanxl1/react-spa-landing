194192f9b1b970a853ca4a30cc7294ae902c2b31
Jonathan A <jonathanxl1@hotmail.com>
2024-10-10 17:23:12 -0500

    refactor:Update Router file

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout";
 import Home from "../pages/Home";
+import About from "../pages/About";
+import Download from "../pages/Download";
+import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -9,6 +14,22 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <Home></Home>,
+      },
+      {
+        path: "about",
+        element: <About></About>,
+      },
+      {
+        path: "download",
+        element: <Download></Download>,
+      },
+      {
+        path: "contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
       },
     ],
   },
