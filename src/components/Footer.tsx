ce709743950fe9590fa9040eab68ca487821ab40
Jonathan A <jonathanxl1@hotmail.com>
2024-10-10 17:28:31 -0500

    feat: Create footer - Added footer layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <footer className="relative bottom-0 w-full h-32 bg-black"></footer>;
+}
+
+export default Footer;
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 function Layout(): JSX.Element {
   return (
     <>
@@ -7,6 +9,7 @@ function Layout(): JSX.Element {
       <main className="w-full h-auto min-h-64 bg-gradient-to-b from-white from-20% to-green-600">
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
